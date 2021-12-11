c8a79e433a62ed9d73ac50b9f41f1f888c3e65c7
Jefry Navas <jnavass@est.ups.edu.ec>
2021-12-11 15:19:25 -0500

    Login interfaz

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  FormGroup,
+    FormControl,
+    Validators,
+    FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.formularioLogin = this.fb.group({
+      nombreP: new FormControl('',Validators.required),
+      nombreH: new FormControl('',Validators.required),
+      usuario: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required)
+    });
+   }
 
   ngOnInit() {
   }
