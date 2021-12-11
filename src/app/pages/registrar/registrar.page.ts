fcf1823edd2a9d139b4c134c3e2becb2f4084ea3
Jefry Navas <jnavass@est.ups.edu.ec>
2021-12-11 15:45:22 -0500

    Login y Registrar

--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -7,8 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  formularioRegistrar: FormGroup;
 
+  constructor(public fb: FormBuilder) {
+    this.formularioRegistrar = this.fb.group({
+      nombreP: new FormControl('',Validators.required),
+      nombreH: new FormControl('',Validators.required),
+      usuario: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required)
+    });
+   }
   ngOnInit() {
   }
 
